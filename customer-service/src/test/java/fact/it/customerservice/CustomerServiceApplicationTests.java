318d3d47738bea93cd9d729f182db03dcef11ba8
unknown <r0892566rstudent.thomasmore.be>
2023-09-28 15:44:55 +0200

    Updated

---
+++ customer-service/src/test/java/fact/it/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ room-service/src/main/java/fact/it/roomservice/RoomServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.roomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomServiceApplication.class, args);
+	}
+
+}
