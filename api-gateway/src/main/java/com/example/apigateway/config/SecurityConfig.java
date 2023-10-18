808599418ef1c155fc28bbf9d105825a84d313eb
Elias Grinwis <r0892566@student.thomasmore.be>
2023-10-18 14:47:55 +0200

    Google Auth

---
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET,"/hotels")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                );
+        return serverHttpSecurity.build();
+    }
+}
