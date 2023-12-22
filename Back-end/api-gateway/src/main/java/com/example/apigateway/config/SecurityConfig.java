0348a4f4871cbab47e2864ab6990804edd408353
Elias Grinwis <r0892566@student.thomasmore.be>
2023-12-22 16:22:40 +0100

    Updated

--- Back-end/api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ Back-end/api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -1,24 +1,16 @@
 package com.example.apigateway.config;
 
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
-
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -35,20 +27,15 @@ public class SecurityConfig {
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .csrf(csrfSpec -> csrfSpec.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())) // CSRF configuration
-                .cors(withDefaults()); // Enable CORS configuration
+                .cors(corsSpec -> corsSpec.configurationSource(corsConfigurationSource())); // Enable CORS configuration
 
         return http.build();
     }
 
-//    @Bean
-//    public KeyResolver keyResolver() {
-//        return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
-//    }
-
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        //configuration.addAllowedOrigin("http://localhost:3000"); // Add your frontend origin
+        // Add your frontend origin(s)
         configuration.addAllowedOrigin("https://microservices-402412.web.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
