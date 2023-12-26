d3d5de78b84c3d22d7fa1614e43474df2926d4c3
Elias Grinwis Plaat Stultjes <91262453+EliasGrinwis@users.noreply.github.com>
2023-12-26 23:01:25 +0100

    Update RedisConfig.java

--- Back-end/api-gateway/src/main/java/com/example/apigateway/config/RedisConfig.java
+++ Back-end/api-gateway/src/main/java/com/example/apigateway/config/RedisConfig.java
@@ -1,30 +1,30 @@
-//package com.example.apigateway.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-//
-//@Configuration
-//@EnableRedisRepositories
-//public class RedisConfig {
-//
-//    @Bean
-//    public RedisStandaloneConfiguration standaloneConfiguration() {
-//        return new RedisStandaloneConfiguration("redis", 6379);
-//    }
-//
-//    @Bean
-//    public LettuceConnectionFactory lettuceConnectionFactory() {
-//        return new LettuceConnectionFactory(standaloneConfiguration());
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(lettuceConnectionFactory());
-//        return template;
-//    }
-//}
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedisStandaloneConfiguration standaloneConfiguration() {
+        return new RedisStandaloneConfiguration("redis", 6379);
+   }
+
+   @Bean
+   public LettuceConnectionFactory lettuceConnectionFactory() {
+       return new LettuceConnectionFactory(standaloneConfiguration());
+    }
+
+  @Bean
+   public RedisTemplate<String, Object> redisTemplate() {
+       RedisTemplate<String, Object> template = new RedisTemplate<>();
+       template.setConnectionFactory(lettuceConnectionFactory());
+      return template;
+    }
+}
